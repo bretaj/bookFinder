46c793bc438f41f9df069de7ff39b8a3aa87d667
bret jensen <jensenbret13@gmail.com>
2025-04-17 21:42:19 -0700

    routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 // TODO: implement Apoolo Server and apply it to the Express server as middleware
+import type { Request, Response } from 'express';
 import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
@@ -34,10 +35,11 @@ app.use('/graphql', expressMiddleware(server as any,
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
 }
-
-// ????
 
 app.use(routes);
 
